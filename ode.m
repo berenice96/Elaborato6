18f482580cfdac0212868438c24045a98907c59d
Francesco <liga97@live.it>
2019-05-14 16:07:19 +0200

    Risoluzione equazione SIR

--- ode.m
+++ ode.m
@@ -1,9 +1,11 @@
-y0 = [199 1 0];
-[t x] = ode45(@prob,[0 20],y0);
-n = length(t)
-figure(1);
-plot(t,x(:,1),'*',t,x(:,2),'*');
-figure(2);
-plot(x(:,1),x(:,2))
-figure(3);
-plot(x(:,1),x(:,3))
+y0 = [199;1;0];
+opt=odeset('RelTol',1e-4);
+a = [0.005;0.01;0.05;0.1];
+
+for i = 1:length(a)
+    [t, x] = ode45(@SIR,[0 20],y0,opt,a(i));
+    picco=max(x(:,2));
+    tpicco=t(x(:,2)==picco);
+    figure('Name',['ode con a = ' num2str(a(i),5),'.']);
+    plot(t,x(:,1),t,x(:,2),t,x(:,3),tpicco, picco, "o"); legend("suscettibili", "infetti" , "immuni");
+end
